b3628cd298f530bb9e58ab4caea5ea379d02d1f4
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-08 17:36:25 -0500

    feat: global loading components

---
+++ components/global/loading-page.tsx
@@ -0,0 +1,9 @@
+import Loading from './loading';
+
+export default function LoadingPage() {
+  return (
+    <div className='flex h-full w-full items-center justify-center'>
+      <Loading />
+    </div>
+  );
+}
