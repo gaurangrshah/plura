bdd28609efce954d0f52ef25c2cc153c6d578241
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-08 02:35:17 -0500

    chore: update middleware routing strategy

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,48 @@
+import { NextResponse } from 'next/server';
+
 import { authMiddleware } from '@clerk/nextjs';
 
 // See https://clerk.com/docs/references/nextjs/auth-middleware -> configuring your Middleware
 export default authMiddleware({
   publicRoutes: ['/site', '/api/uploadthing'],
+  async beforeAuth(auth, req) {},
+  async afterAuth(auth, req) {
+    // rewrite for domains
+    const url = req.nextUrl;
+    const searchParams = url.searchParams.toString();
+    let hostname = req.headers;
+
+    const pathWithSearchParams = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ''}`;
+
+    const customSubdomain = hostname
+      .get('host')
+      ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+      .filter(Boolean)[0];
+
+    if (customSubdomain) {
+      return NextResponse.rewrite(
+        new URL(`/${customSubdomain}${pathWithSearchParams}`, req.url)
+      );
+    }
+
+    if (url.pathname === '/sign-in' || url.pathname === '/sign-up') {
+      return NextResponse.redirect(new URL(`/agency/sign-in`, req.url));
+    }
+
+    if (
+      url.pathname === '/' ||
+      (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+    ) {
+      return NextResponse.rewrite(new URL(`/site`, req.url));
+    }
+
+    if (
+      url.pathname.startsWith('/agency') ||
+      url.pathname.startsWith('/subaccount')
+    ) {
+      return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url));
+    }
+  },
 });
 
 export const config = {
