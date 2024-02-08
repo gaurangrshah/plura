42d1154d24905be4ef2ae30966b124252d902acd
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-08 16:06:34 -0500

    feat: dashboard auth + create-agency scaffold

---
+++ components/forms/agency-details.tsx
@@ -0,0 +1,122 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Agency } from '@prisma/client';
+import { AlertDialog } from '@radix-ui/react-alert-dialog';
+
+import FileUpload from '../global/file-upload';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { useToast } from '../ui/use-toast';
+
+type AgencyDetailProps = {
+  data?: Partial<Agency>;
+};
+
+const FormSchema = z.object({
+  name: z.string().min(2, { message: 'Agency name must be atleast 2 chars.' }),
+  companyEmail: z.string().min(1),
+  companyPhone: z.string().min(1),
+  whiteLabel: z.boolean(),
+  address: z.string().min(1),
+  city: z.string().min(1),
+  zipCode: z.string().min(1),
+  state: z.string().min(1),
+  country: z.string().min(1),
+  agencyLogo: z.string().min(1),
+});
+
+export function AgencyDetails({ data }: AgencyDetailProps) {
+  /* ------------------------------------------------------------------------ */
+  /*                                  Config                                */
+  /* ------------------------------------------------------------------------ */
+  const toast = useToast();
+  const router = useRouter();
+
+  const [deletingAgency, setDeletingAgency] = useState(false);
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    mode: 'onChange',
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: data?.name,
+      companyEmail: data?.companyEmail,
+      companyPhone: data?.companyPhone,
+      whiteLabel: data?.whiteLabel || false,
+      address: data?.address,
+      city: data?.city,
+      zipCode: data?.zipCode,
+      state: data?.state,
+      country: data?.country,
+      agencyLogo: data?.agencyLogo,
+    },
+  });
+  const isLoading = form.formState.isSubmitting;
+
+  useEffect(() => data && form.reset(data), [data, form]);
+
+  /* ------------------------------------------------------------------------ */
+  /*                                  Handlers                                */
+  /* ------------------------------------------------------------------------ */
+
+  const handleSubmit = async () => {};
+
+  return (
+    <AlertDialog>
+      <Card className='w-full'>
+        <CardHeader>
+          <CardTitle>Agency Information</CardTitle>
+          <CardDescription>
+            Lets create an agency for you business. You can edit agency settings
+            later from the agency settings tab.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className='space-y-4'
+            >
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name='agencyLogo'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Agency Logo</FormLabel>
+                    <FormControl>
+                      <FileUpload
+                        apiEndpoint='agencyLogo'
+                        onChange={field.onChange}
+                        value={field.value}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </AlertDialog>
+  );
+}
