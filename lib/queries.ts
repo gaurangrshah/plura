fc2805e7952214e3a67f8726c0c2446239a96e83
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-08 18:52:19 -0500

    api: initUser + upsertAgency

--- lib/queries.ts
+++ lib/queries.ts
@@ -3,7 +3,7 @@
 import { redirect } from 'next/navigation';
 
 import { clerkClient, currentUser } from '@clerk/nextjs';
-import { Agency, User } from '@prisma/client';
+import { Agency, Plan, User } from '@prisma/client';
 
 import db from './db';
 
@@ -165,7 +165,86 @@ export const updateAgencyDetails = async (
   return response;
 };
 
+export const upsertAgency = async (agency: Agency, price?: Plan) => {
+  if (!agency.companyEmail) return null;
+  try {
+    const agencyDetails = await db.agency.upsert({
+      where: {
+        id: agency.id,
+      },
+      update: agency,
+      create: {
+        users: {
+          connect: { email: agency.companyEmail },
+        },
+        ...agency,
+        SidebarOption: {
+          create: [
+            {
+              name: 'Dashboard',
+              icon: 'category',
+              link: `/agency/${agency.id}`,
+            },
+            {
+              name: 'Launchpad',
+              icon: 'clipboardIcon',
+              link: `/agency/${agency.id}/launchpad`,
+            },
+            {
+              name: 'Billing',
+              icon: 'payment',
+              link: `/agency/${agency.id}/billing`,
+            },
+            {
+              name: 'Settings',
+              icon: 'settings',
+              link: `/agency/${agency.id}/settings`,
+            },
+            {
+              name: 'Sub Accounts',
+              icon: 'person',
+              link: `/agency/${agency.id}/all-subaccounts`,
+            },
+            {
+              name: 'Team',
+              icon: 'shield',
+              link: `/agency/${agency.id}/team`,
+            },
+          ],
+        },
+      },
+    });
+    return agencyDetails;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const deleteAgency = async (agencyId: string) => {
   const response = await db.agency.delete({ where: { id: agencyId } });
   return response;
+};
+
+export const initUser = async (newUser: Partial<User>) => {
+  const user = await currentUser();
+  if (!user) return;
+
+  const updateUserData = await db.user.upsert({
+    where: { email: user.emailAddresses[0].emailAddress },
+    update: newUser,
+    create: {
+      id: user.id,
+      avatarUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+      ...newUser,
+      name: `${user.firstName} ${user.lastName}`,
+      role: newUser.role || 'SUBACCOUNT_USER',
+    },
+  });
+
+  await clerkClient.users.updateUserMetadata(user.id, {
+    privateMetadata: {
+      role: newUser.role || 'SUBACCOUNT_USER',
+    },
+  });
 };
