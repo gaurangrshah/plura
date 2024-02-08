aaf02bbe2997c48e3893fa9ac8fa4508447a8a2d
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-08 17:26:09 -0500

    api: add updateAgencyQuery

--- lib/queries.ts
+++ lib/queries.ts
@@ -3,7 +3,7 @@
 import { redirect } from 'next/navigation';
 
 import { clerkClient, currentUser } from '@clerk/nextjs';
-import { User } from '@prisma/client';
+import { Agency, User } from '@prisma/client';
 
 import db from './db';
 
@@ -151,4 +151,16 @@ export const verifyAndAcceptInvitation = async () => {
 
     return agency ? agency.agencyId : null;
   }
+};
+
+export const updateAgencyDetails = async (
+  agencyId: string,
+  agencyDetails: Partial<Agency>
+) => {
+  const response = await db.agency.update({
+    where: { id: agencyId },
+    data: { ...agencyDetails },
+  });
+
+  return response;
 };
