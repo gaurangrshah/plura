e4c7fc5b672698f3ea532fbc518430e2cdd87ad6
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-08 16:49:54 -0500

    chore: configure uploadthing

---
+++ lib/uploadthing.ts
@@ -0,0 +1,18 @@
+import {
+  generateComponents,
+  generateUploadButton,
+  generateUploadDropzone,
+  generateUploader,
+} from '@uploadthing/react';
+import { generateReactHelpers } from '@uploadthing/react/hooks';
+
+import type { OurFileRouter } from '@/app/api/uploadthing/core';
+
+export const UploadButton = generateUploadButton<OurFileRouter>();
+
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+
+export const Uploader = generateUploader<OurFileRouter>();
+
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<OurFileRouter>();
