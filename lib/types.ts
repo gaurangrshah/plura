9beb8010135bfe82f365aba8886e1f606a2b0b4a
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-09 17:35:02 -0500

    chore: add types for subacct user permissions query

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,13 @@
-import { Contact, Lane, Notification, Prisma, Role, Tag, Ticket, User } from '@prisma/client';
+import {
+  Notification,
+  Prisma,
+  Role,
+} from '@prisma/client';
+
+import {
+  getAuthUserDetails,
+  getUserPermissions,
+} from './queries';
 
 export type NotificationWithUser =
   | ({
@@ -14,3 +23,10 @@ export type NotificationWithUser =
       };
     } & Notification)[]
   | undefined;
+
+export type UserWithPermissionsAndSubAccounts = Prisma.PromiseReturnType<
+  typeof getUserPermissions
+>;
+
+export type AuthUserWithAgencySidebarOptionsSubAccounts =
+  Prisma.PromiseReturnType<typeof getAuthUserDetails>;
