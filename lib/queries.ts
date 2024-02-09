0206d5956040809f46825271230d22e533a6bfb7
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-08 21:03:46 -0500

    api: getNotificationAndUser

--- lib/queries.ts
+++ lib/queries.ts
@@ -251,3 +251,18 @@ export const initUser = async (newUser: Partial<User>) => {
 
   return userData;
 };
+
+export const getNotificationAndUser = async (agencyId: string) => {
+  try {
+    const response = await db.notification.findMany({
+      where: { agencyId },
+      include: { User: true },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
