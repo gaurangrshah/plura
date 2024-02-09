65851aab8a38c483adff8a03804cb38f63a5c20f
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-09 14:09:33 -0500

    chore: remove unused deps

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { cva, type, VariantProps } from 'class-variance-authority';
+import { cva, VariantProps } from 'class-variance-authority';
 import { X } from 'lucide-react';
 import * as React from 'react';
 
