18d8c6bc40aa1c9c3483cfa2983dbd1f613b6865
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-10 00:09:50 -0500

    feat: team page + wip: send invitation

---
+++ app/(main)/agency/[agencyId]/team/page.tsx
@@ -0,0 +1,56 @@
+import { currentUser } from '@clerk/nextjs';
+
+import { Plus } from 'lucide-react';
+
+import { SendInvitation } from '@/components/forms/send-invitation';
+
+import db from '@/lib/db';
+
+import { columns } from './columns';
+import DataTable from './data-table';
+
+type Props = {
+  params: { agencyId: string };
+};
+
+export default async function TeamPage({ params }: Props) {
+  const authUser = await currentUser();
+  const teamMembers = await db.user.findMany({
+    where: {
+      Agency: {
+        id: params.agencyId,
+      },
+    },
+    include: {
+      Agency: { include: { SubAccount: true } },
+      Permissions: { include: { SubAccount: true } },
+    },
+  });
+
+  if (!authUser) return null;
+  const agencyDetails = await db.agency.findUnique({
+    where: {
+      id: params.agencyId,
+    },
+    include: {
+      SubAccount: true,
+    },
+  });
+
+  if (!agencyDetails) return;
+
+  return (
+    <DataTable
+      actionButtonText={
+        <>
+          <Plus size={15} />
+          Add
+        </>
+      }
+      modalChildren={<SendInvitation agencyId={agencyDetails.id} />}
+      filterValue='name'
+      columns={columns}
+      data={teamMembers}
+    />
+  );
+}
