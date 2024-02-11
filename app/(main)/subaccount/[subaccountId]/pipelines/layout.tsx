601c5d9d7b5bc24db33455af03b21867c4e8c4af
gshah2020 <gaurang.r.shah@gmail.com>
2024-02-10 22:24:43 -0500

    feat: wip: pipelines list page layout + loading state

---
+++ app/(main)/subaccount/[subaccountId]/pipelines/layout.tsx
@@ -0,0 +1,5 @@
+import { BlurPage } from '@/components/global/blur-page';
+
+export default function PipelinesLayout({ children }: { children: React.ReactNode} ) {
+  return <BlurPage>{children}</BlurPage>
+}
---
+++ app/(main)/subaccount/[subaccountId]/pipelines/loading.tsx
@@ -0,0 +1,7 @@
+import Loading from '@/components/global/loading';
+
+export default function PipelinesLoadingPage() {
+  return (
+    <div className="h-screen -mt-8"><Loading /></div>
+  )
+}
